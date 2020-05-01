dc32e8fefc954352ba658f1dca384481d4870ea5
Gio Justiniano <giojustiniano@Gios-Air.home>
2020-05-01 04:17:14 +0200

    Updates

---
+++ precache-manifest.6a90e66b906b03cb76fbf146dca34db8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e94a9f2ba1df54ec69730404577572e",
+    "url": "/brastlewark_gnome_directory/index.html"
+  },
+  {
+    "revision": "09e9bc5226cbb356abd0",
+    "url": "/brastlewark_gnome_directory/static/css/main.eef850d1.chunk.css"
+  },
+  {
+    "revision": "642c850769b761e706f7",
+    "url": "/brastlewark_gnome_directory/static/js/2.0644a93a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brastlewark_gnome_directory/static/js/2.0644a93a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09e9bc5226cbb356abd0",
+    "url": "/brastlewark_gnome_directory/static/js/main.af0ef2f8.chunk.js"
+  },
+  {
+    "revision": "a1e6a9ec2b3cdbb507d5",
+    "url": "/brastlewark_gnome_directory/static/js/runtime-main.896a1fb4.js"
+  }
+]);
